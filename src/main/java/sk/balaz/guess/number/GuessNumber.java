7844aa883ddaa2aafbc327f1d88fd5cdc4818b25
Tomas Balaz <tomas.balaz@asseco-ce.com>
2018-02-23 13:39:34 +0100

    Create GuessNumber class

---
+++ src/main/java/sk/balaz/guess/number/GuessNumber.java
@@ -0,0 +1,13 @@
+package sk.balaz.guess.number;
+
+/**
+ * Hello world!
+ *
+ */
+public class GuessNumber 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
