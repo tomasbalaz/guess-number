e64b184174038f2827359730baf1ac6da58ea3a2
Tomas Balaz <tomas.balaz@asseco-ce.com>
2018-02-23 13:49:22 +0100

    Create basic skeleton for Guess number game

--- src/main/java/sk/balaz/guess/number/GuessNumber.java
+++ src/main/java/sk/balaz/guess/number/GuessNumber.java
@@ -1,13 +1,20 @@
 package sk.balaz.guess.number;
 
-/**
- * Hello world!
- *
- */
 public class GuessNumber 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+	public void guess(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isWon() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
--- src/test/java/sk/balaz/guess/number/GuessNumberTest.java
+++ src/test/java/sk/balaz/guess/number/GuessNumberTest.java
@@ -1,38 +1,15 @@
 package sk.balaz.guess.number;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class GuessNumberTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public GuessNumberTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( GuessNumberTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class GuessNumberTest  {
+	
+	@Test
+	public void testIdealRun() {
+		GuessNumber guessNumber = new GuessNumber();
+		
+		while(!guessNumber.isWon()) {
+			guessNumber.guess(1);
+		}
+	}
 }
